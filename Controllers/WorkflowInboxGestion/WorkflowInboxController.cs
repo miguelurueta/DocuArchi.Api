f69114bea506daafd29f1a6125d55f5fa84c21a2
maum <maum_maum@hotmail.com>
2026-04-07 18:39:53 -0500

    SCRUM-132 expone endpoint autocomplete workflow

--- Controllers/WorkflowInboxGestion/WorkflowInboxController.cs
+++ Controllers/WorkflowInboxGestion/WorkflowInboxController.cs
@@ -58,6 +58,39 @@ namespace DocuArchi.Api.Controllers.WorkflowInboxGestion
             return Ok(result);
         }
 
+        [HttpPost("inboxgestion/autocomplete")]
+        public async Task<ActionResult<AppResponses<WorkflowInboxAutocompleteResponseDto>>> AutocompleteBandejaWorkflow(
+            [FromBody] WorkflowInboxAutocompleteRequestDto request)
+        {
+            var validation = _claimValidationService.ValidateClaim<string>("defaulalias");
+            if (!validation.Success || validation.ClaimValue == null)
+            {
+                return BadRequest(validation.Response);
+            }
+
+            var validationUsuario = _claimValidationService.ValidateClaim<string>("usuarioid");
+            if (!validationUsuario.Success || validationUsuario.ClaimValue == null)
+            {
+                return BadRequest(validationUsuario.Response);
+            }
+
+            if (!int.TryParse(validationUsuario.ClaimValue, out var idUsuarioGestion))
+            {
+                throw new SecurityException("Claim invalido: usuarioid");
+            }
+
+            var result = await _workflowInboxService.AutocompleteBandejaWorkflowAsync(
+                request,
+                idUsuarioGestion,
+                validation.ClaimValue);
+            if (!result.success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
         [HttpPost("/api/AppTable/export")]
         public async Task<ActionResult> ExportaBandejaWorkflow(
             [FromBody] WorkflowInboxExportRequestDto request)
